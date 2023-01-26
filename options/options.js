ea4d33474897bb33cf27bb275324fa564ea43fce
Mohd Irfan <mohd.irfan@bambooapp.com>
2023-01-26 20:38:38 +0530

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "second-app-38491",
   url: "https://second-app-38491.botics.co",
   api: "https://second-app-38491.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
